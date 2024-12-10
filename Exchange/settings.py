54a6e75ea77b391808909bdedbdab162b96db5cd
crypto <crypto@hmdkh.ir>
2024-12-10 09:47:27 +0000

    whitenoise.middleware.WhiteNoiseMiddleware

--- Exchange/settings.py
+++ Exchange/settings.py
@@ -58,6 +58,7 @@ Q_CLUSTER = {
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
